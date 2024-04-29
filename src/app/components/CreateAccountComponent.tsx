70280c70b7c65372d036dce75d72ba3a5b80483c
JosephPoncini <jxponcini@gmail.com>
2024-04-29 11:28:35 -0700

    create account page change

--- src/app/components/CreateAccountComponent.tsx
+++ src/app/components/CreateAccountComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CreateAccountComponent = () => {
   return (
-    <div className=' bg-dark-teal flex flex-col items-center w-[730px] h-[100%] py-[51px] px-[25px]'>
+    <div className=' bg-dark-teal flex flex-col items-center w-[730px] h-[100%] py-[51px] px-[25px] space-y-24'>
       <div className=' font-HoltwoodOneSC text-5xl'>Create Account</div>
         <div className=' w-[595px] flex'>
           <div className=''>Username</div>
