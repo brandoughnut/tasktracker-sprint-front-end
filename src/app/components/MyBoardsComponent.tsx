84610ed5e7d4be6398a62570cd19229ec62e9976
brandoughnut <brandonnguyen2867@gmail.com>
2024-04-29 10:51:35 -0700

    created board component

---
+++ src/app/components/MyBoardsComponent.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const MyBoardsComponent = () => {
+  return (
+    <div className='bg-white border-[1px] border-black rounded-[5px] h-[789px] w-[800px]'>
+      
+    </div>
+  )
+}
+
+export default MyBoardsComponent
--- src/app/pages/DashboardPage/page.tsx
+++ src/app/pages/DashboardPage/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 
+import MyBoardsComponent from '@/app/components/MyBoardsComponent'
 import NavBarComponent from '@/app/components/NavBarComponent'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const DashboardPage = () => {
+
+  useEffect(() => {
+    document.body.style.backgroundColor = '#F1FFFC'
+  }, [])
+
   return (
     <div>
       <NavBarComponent/>
+      <MyBoardsComponent/>
     </div>
   )
 }
