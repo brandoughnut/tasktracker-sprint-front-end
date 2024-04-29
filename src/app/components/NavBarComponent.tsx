76947b17ab938d5728f60df35a0b2b1697373d94
brandoughnut <brandonnguyen2867@gmail.com>
2024-04-29 11:20:45 -0700

    created board

--- src/app/components/NavBarComponent.tsx
+++ src/app/components/NavBarComponent.tsx
@@ -7,18 +7,19 @@ import Image from 'next/image';
 const NavBarComponent = () => {
   return (
     <div className='h-[135px] w-full bg-gradient-to-r from-[#AEE6D9] to-[#3EBE9F]'>
-      <div className='h-full flex mx-[36px]'>
-        <div className='my-auto'>
-          <div className='text-center HoltwoodOneSC'>
+      <div className='h-full flex mx-[36px] justify-between'>
+        <div className='flex'>
+          <div className='text-center HoltwoodOneSC my-auto'>
             <p className='text-black text-[32px] leading-none'>Maddie</p>
             <p className='text-[#0B7D61] text-[28px] leading-none'>is cool</p>
           </div>
-        </div>
         <Image className='h-[96.94px] mt-3.5' src={navbarlogo} alt='exclamation mark'/>
-      <div>
+        </div>
+        <div className='my-auto h-[66px] w-[67px] rounded-full border-black border-[1px] bg-white'>
 
       </div>
       </div>
+      
       
       
     </div>
