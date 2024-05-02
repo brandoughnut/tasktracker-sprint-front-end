6a219c14b242b28388ee356d15255bc471f779ab
JosephPoncini <jxponcini@gmail.com>
2024-05-02 09:23:43 -0700

    fixed indefinite loop

--- src/app/components/BoardCardComponent.tsx
+++ src/app/components/BoardCardComponent.tsx
@@ -1,28 +1,31 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 
-const BoardCardComponent = (prop: {projectName: string; colorID: number}) => {
+const BoardCardComponent = (props: {projectName: string; colorID: number}) => {
 
   const [className, setClassName] = useState<string>('cursor-pointer HammersmithOne text-[40px] w-full h-[117px] flex justify-between mt-[40px] mb-[60px]')
 
-  switch(prop.colorID){
-    case 1:
-      setClassName(className + ' bg-teal')
-      break;
-    case 2:
-      setClassName(className + ' bg-dark-teal')
-      break;
-    default:
-      setClassName(className + ' bg-light-teal')
-      break;
-  }
+
+  useEffect(() => {
+    switch (props.colorID) {
+      case 1:
+        setClassName((prevClassName) => prevClassName + ' bg-teal');
+        break;
+      case 2:
+        setClassName((prevClassName) => prevClassName + ' bg-dark-teal');
+        break;
+      default:
+        setClassName((prevClassName) => prevClassName + ' bg-light-teal');
+        break;
+    }
+  }, [props.colorID]);
 
   return (
     <div className={className}>
-      <div className='my-auto ms-[34px]'><h1>{prop.projectName}</h1></div>
+      <div className='my-auto ms-[34px]'><h1>{props.projectName}</h1></div>
       <div className='my-auto me-[48px] rounded-full bg-white h-[50px] w-[50px] border-black border-[1px]'></div>
     </div>
   )
